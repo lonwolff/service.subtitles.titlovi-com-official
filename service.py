48b3b95a9281e075fd403b4d219557835d370ea6
tomislav <tturka33@gmail.com>
2017-12-03 22:29:21 +0100

    bug fixes, updated manual search

--- service.py
+++ service.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
 import os
-import shutil
 import sys
+import re
 import xbmc
 import xbmcaddon
 import xbmcgui
@@ -12,11 +12,9 @@ import requests
 import simplecache
 import time
 import unicodedata
-import zipfile
 import StringIO
 from datetime import datetime, timedelta
 from urlparse import parse_qs
-from os.path import basename
 from zipfile import ZipFile
 
 addon = xbmcaddon.Addon()
@@ -48,14 +46,53 @@ if not xbmcvfs.exists(temp_dir):
 
 addon_cache = simplecache.SimpleCache()
 
+
 def logger(message):
     xbmc.log(u"{0} - {1}".format(__name__, message).encode('utf-8'))
+
 
 def show_notification(message):
     xbmc.executebuiltin('Notification({0}, {1})'.format(script_name, message))
 
+
 def normalize_string(_string):
     return unicodedata.normalize('NFKD', unicode(_string, 'utf-8')).encode('ascii', 'ignore')
+
+
+def parse_season_episode(_string):
+    """
+    Function used for parsing season and episode numbers from string.
+    If season and episode are found they are stripped from original string.
+    Allowed format is 'S01E01'. Allowed number range is 00-99.
+    Returns three-tuple.
+    """
+
+    if not _string:
+        return _string, None, None
+    results = re.findall('[sS](\d{2})[eE](\d{2})', _string)
+    if not results:
+        return _string, None, None
+    try:
+        season = int(results[0][0])
+        if season < 0 or season > 99:
+            season = None
+    except Exception as e:
+        logger(e)
+        season = None
+
+    try:
+        episode = int(results[0][1])
+        if episode < 0 or episode > 99:
+            episode = None
+    except Exception as e:
+        logger(e)
+        episode = None
+
+    if season is not None and episode is not None:
+        _string = _string.lower().replace('s{0}e{1}'.format(results[0][0], results[0][1]), '')
+
+    return _string, season, episode
+
 
 language_mapping = {
     'English': 'English',
@@ -75,6 +112,7 @@ language_icon_mapping = {
     'Bosanski': 'bs'
 }
 
+
 class ActionHandler(object):
     def __init__(self, params):
         """
@@ -92,6 +130,7 @@ class ActionHandler(object):
         self.password = addon.getSetting("titlovi-password")
         self.action = self.params['?action'][0]
         self.login_token = None
+        self.user_id = None
 
     def validate_params(self):
         """
@@ -104,7 +143,7 @@ class ActionHandler(object):
             show_notification(get_string(2103))
             return False
         return True
-    
+
     def get_prepared_language_param(self):
         """
         Method used for parsing chosen subtitle languages and formatting them in format accepted by Titlovi.com API.
@@ -121,17 +160,17 @@ class ActionHandler(object):
                 if language_mapping['Croatian'] not in lang_list:
                     lang_list.append(language_mapping['Croatian'])
             else:
-                if language_mapping[lang] not in lang_list:
+                if lang in language_mapping and language_mapping[lang] not in lang_list:
                     lang_list.append(language_mapping[lang])
-                   
+
         lang_string = '|'.join(lang_list)
-        
+
         return lang_string
 
     def handle_login(self):
         """
         Method used for sending user login request.
-        
+
         OK return:
             {
                 "ExpirationDate": datetime string (format: '%Y-%m-%dT%H:%M:%S.%f'),
@@ -146,7 +185,7 @@ class ActionHandler(object):
         login_params = dict(username=self.username, password=self.password, json=True)
         try:
             response = requests.post('{0}/gettoken'.format(api_url), params=login_params)
-            logger('Response status: {0}'.format(response.status_code))  
+            logger('Response status: {0}'.format(response.status_code))
             if response.status_code == requests.codes.ok:
                 resp_json = response.json()
                 logger('login response data: {0}'.format(resp_json))
@@ -179,11 +218,11 @@ class ActionHandler(object):
                 return False
             self.set_login_data(login_data)
             return True
-        
+
         logger('user login data found in cache: {0}'.format(titlovi_com_login_data))
         expiration_date_string = titlovi_com_login_data.get('ExpirationDate')
-        expiration_date = datetime(*(time.strptime(expiration_date_string,  '%Y-%m-%dT%H:%M:%S.%f')[0:6]))
-        #expiration_date = datetime.strptime(expiration_date_string, '%Y-%m-%dT%H:%M:%S.%f')
+        expiration_date = datetime(*(time.strptime(expiration_date_string, '%Y-%m-%dT%H:%M:%S.%f')[0:6]))
+        # expiration_date = datetime.strptime(expiration_date_string, '%Y-%m-%dT%H:%M:%S.%f')
         date_delta = expiration_date - datetime.now()
         if date_delta.days <= 1:
             login_data = self.handle_login()
@@ -192,10 +231,10 @@ class ActionHandler(object):
                 return False
             self.set_login_data(login_data)
             return True
-        
+
         self.set_login_data(titlovi_com_login_data)
         return True
-    
+
     def handle_action(self):
         """
         Method used for calling other action methods depending on 'action' parameter.
@@ -208,19 +247,36 @@ class ActionHandler(object):
         else:
             logger(u'Invalid action')
             show_notification(get_string(2103))
-        
+
     def handle_search_action(self):
         """
         Method used for searching
-          """
+        """
         logger('starting search')
         search_params = {}
         if self.action == 'manualsearch':
+            logger('starting manualsearch')
             search_string = self.params.get('searchstring')
             if not search_string:
                 show_notification(get_string(32008))
                 return
-            search_params['query'] = search_string
+
+            search_string = search_string[0]
+            if not search_string:
+                show_notification(get_string(32008))
+                return
+            logger('search_string {0}'.format(search_string))
+
+            search_string, season, episode = parse_season_episode(search_string)
+
+            if season is not None:
+                search_params['season'] = season
+            if episode is not None:
+                search_params['episode'] = episode
+            clean_title, year = xbmc.getCleanMovieTitle(search_string)
+            search_params['query'] = clean_title
+            logger(search_params)
+
         else:
             imdb_id = video_info.getIMDBNumber()
             if imdb_id:
@@ -231,26 +287,39 @@ class ActionHandler(object):
             tv_show_title = normalize_string(xbmc.getInfoLabel("VideoPlayer.TVshowtitle"))
             if tv_show_title:
                 search_params['query'] = tv_show_title
-                if season:
-                    search_params['season'] = season 
-                if episode:
+                if season is not None:
+                    search_params['season'] = season
+                if episode is not None:
                     search_params['episode'] = episode
             else:
                 title = normalize_string(xbmc.getInfoLabel("VideoPlayer.OriginalTitle"))
                 if not title:
                     title = normalize_string(xbmc.getInfoLabel("VideoPlayer.Title"))
-                
+                # TODO parse season/epizode info
                 if title:
+                    title, season, episode = parse_season_episode(title)
+                    if season is not None:
+                        search_params['season'] = season
+                    if episode is not None:
+                        search_params['episode'] = episode
+
                     clean_title, year = xbmc.getCleanMovieTitle(title)
                     search_params['query'] = clean_title
                 else:
+                    current_video_name = player.getPlayingFile()
+                    current_video_name, season, episode = parse_season_episode(current_video_name)
+                    if season is not None:
+                        search_params['season'] = season
+                    if episode is not None:
+                        search_params['episode'] = episode
+
                     try:
-                        current_video_name, year = xbmc.getCleanMovieTitle(player.getPlayingFile())
+                        clean_video_name, year = xbmc.getCleanMovieTitle(current_video_name)
                     except Exception as e:
                         logger(e)
                         show_notification(get_string(32009))
                         return
-                    search_params['query'] = current_video_name
+                    search_params['query'] = clean_video_name
 
         search_language = self.get_prepared_language_param()
         if search_language:
@@ -270,67 +339,70 @@ class ActionHandler(object):
             search_params['json'] = True
             result_list_page = 1
             result_list = []
-            while True:
-                search_params['pg'] = result_list_page
-                logger('search params: {0}'.format(search_params))
-                try:
+            logger('search params: {0}'.format(search_params))
+            try:
+                response = requests.get('{0}/search'.format(api_url), params=search_params)
+                logger('Response status code: {0}'.format(response.status_code))
+                if response.status_code == requests.codes.ok:
+                    resp_json = response.json()
+                elif response.status_code == requests.codes.unauthorized:
+                    """
+                    Force user login in case of Unauthorized response
+                    and retry search with new login token
+                    """
+                    is_loggedin = self.user_login()
+                    if not is_loggedin:
+                        logger('Force login failed, exiting!')
+                        return
+                    search_params['token'] = self.login_token
+                    search_params['userid'] = self.user_id
                     response = requests.get('{0}/search'.format(api_url), params=search_params)
-                    logger('Response status code: {0}'.format(response.status_code))
                     if response.status_code == requests.codes.ok:
                         resp_json = response.json()
-                    elif resp_json.status_code == requests.codes.unauthorized:
-                        # force user login in case of Unauthorized response
-                        is_loggedin = self.user_login()
-                        if not is_loggedin:
-                            logger('Force login failed, exiting!')
-                            return
-                        search_params['token'] = self.login_token
-                        search_params['userid'] = self.user_id
-                        response = requests.get('{0}/search'.format(api_url), params=search_params)
-                        if response.status_code == requests.codes.ok:
-                            resp_json = response.json()
-                        else:
-                            logger('Invalid response status code, exiting!')
-                            return 
                     else:
+                        show_notification(get_string(32001))
                         logger('Invalid response status code, exiting!')
-                        return 
-
-                    if resp_json['SubtitleResults']:
-                        result_list.extend(resp_json['SubtitleResults'])
-
-                    if resp_json['PagesAvailable'] > resp_json['CurrentPage']:
-                        result_list_page += 1
-                    else:
-                        break
-                    
-                    if result_list_page == 50:
-                        break
-
-                except Exception as e:
-                    logger(e)
+                        return
+                else:
+                    logger('Invalid response status code, exiting!')
+                    show_notification(get_string(32001))
                     return
+
+                if resp_json['SubtitleResults']:
+                    result_list.extend(resp_json['SubtitleResults'])
+
+            except Exception as e:
+                logger(e)
+                show_notification(get_string(32001))
+                return
 
             if result_list:
                 addon_cache.set(params_hash, result_list, expiration=timedelta(days=3))
 
         for result_item in result_list:
             title = result_item['Title']
+            if result_item['Season'] and result_item['Episode']:
+                title = u'{0} S{1}E{2}'.format(title, result_item['Season'], result_item['Episode'])
+
             if result_item['Release']:
                 title = u'{0} {1}'.format(title, result_item['Release'])
 
             listitem = xbmcgui.ListItem(
-                label=result_item['Lang'], 
-                label2=title, 
+                label=result_item['Lang'],
+                label2=title,
                 iconImage=str(int(result_item['Rating'])),
                 thumbnailImage=language_icon_mapping[result_item['Lang']]
             )
-            url = "plugin://{0}/?action=download&media_id={1}&type={2}"\
+            url = "plugin://{0}/?action=download&media_id={1}&type={2}" \
                 .format(script_id, result_item['Id'], result_item['Type'])
 
             xbmcplugin.addDirectoryItem(handle=plugin_handle, url=url, listitem=listitem, isFolder=False)
 
     def handle_download_action(self):
+        """
+        Method used for downloading subtitle zip file and extracting it.
+        If subtitle file is already downloaded it is reused.
+        """
         subtitle_exists = False
         cache_key = 'titlovi_com_subtitle_{0}_{1}'.format(self.params['media_id'], self.params['type'])
         subtitle_file = addon_cache.get(cache_key)
@@ -342,8 +414,7 @@ class ActionHandler(object):
 
         if not subtitle_exists:
             logger('subtitle_file not found in cache, starting download')
-            request_params = dict(username=self.username, password=self.password)
-            download_url = "https://titlovi.com/download/?type={0}&mediaid={1}"\
+            download_url = "https://titlovi.com/download/?type={0}&mediaid={1}" \
                 .format(self.params['type'][0], self.params['media_id'][0])
             try:
                 logger(download_url)
@@ -358,8 +429,8 @@ class ActionHandler(object):
                 logger(e)
                 show_notification(get_string(32010))
                 return
-            
-            zip_file = zipfile.ZipFile(StringIO.StringIO(response.content))
+
+            zip_file = ZipFile(StringIO.StringIO(response.content))
             zip_contents = zip_file.namelist()
             if not zip_contents:
                 show_notification(get_string(32010))
@@ -373,6 +444,7 @@ class ActionHandler(object):
 
         list_item = xbmcgui.ListItem(label=subtitle_file)
         xbmcplugin.addDirectoryItem(handle=plugin_handle, url=subtitle_file_path, listitem=list_item, isFolder=False)
+
 
 """
 params_dict:
